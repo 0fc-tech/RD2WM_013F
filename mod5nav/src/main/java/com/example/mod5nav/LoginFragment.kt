90d8b5f0a36f441a48565477649ec4ce183c24a3
riderofzion <quentinrouet@hotmail.fr>
2022-11-24 15:00:56 +0100

    Module 5 Nav

---
+++ mod5nav/src/main/java/com/example/mod5nav/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.mod5nav
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.mod5nav.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonSignIn.setOnClickListener {
+            //TODO On se dirige vers Home fragment
+            it.findNavController().navigate(R.id.actionLoginToHome)
+        }
+    }
+}
